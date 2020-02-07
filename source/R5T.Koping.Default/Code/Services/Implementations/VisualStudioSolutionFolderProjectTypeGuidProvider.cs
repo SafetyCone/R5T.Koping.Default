d7e7bca455f830571974e0ac802e5aa87714a8ea
MinexAutomation <minex.automation@gmail.com>
2020-02-06 17:54:39 -0800

    Added VisualStudioSolutionFolderProjectTypeGuidProvider implementation of IVisualStudioSolutionFolderProjectTypeGuidProvider.

---
+++ source/R5T.Koping.Default/Code/Services/Implementations/VisualStudioSolutionFolderProjectTypeGuidProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using R5T.Koping.Base;
+
+
+namespace R5T.Koping.Default
+{
+    public class VisualStudioSolutionFolderProjectTypeGuidProvider : IVisualStudioSolutionFolderProjectTypeGuidProvider
+    {
+        public Guid GetVisualStudioSolutionFolderProjectTypeGuid()
+        {
+            return Constants.SolutionFolderProjectTypeGuid;
+        }
+    }
+}
