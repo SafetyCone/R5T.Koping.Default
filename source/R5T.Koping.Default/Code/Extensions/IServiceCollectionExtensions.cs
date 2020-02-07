b4a1cbfa0952219f5ff3f6b55cb24ec73ba839a2
MinexAutomation <minex.automation@gmail.com>
2020-02-06 17:56:57 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Koping.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.Koping.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFolderProjectTypeGuidProvider"/> implementation of <see cref="IVisualStudioSolutionFolderProjectTypeGuidProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddDefaultVisualStudioSolutionFolderProjectTypeGuidProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<IVisualStudioSolutionFolderProjectTypeGuidProvider, VisualStudioSolutionFolderProjectTypeGuidProvider>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="VisualStudioSolutionFolderProjectTypeGuidProvider"/> implementation of <see cref="IVisualStudioSolutionFolderProjectTypeGuidProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<IVisualStudioSolutionFolderProjectTypeGuidProvider> AddDefaultVisualStudioSolutionFolderProjectTypeGuidProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IVisualStudioSolutionFolderProjectTypeGuidProvider>(() => services.AddDefaultVisualStudioSolutionFolderProjectTypeGuidProvider());
+            return serviceAction;
+        }
+    }
+}
